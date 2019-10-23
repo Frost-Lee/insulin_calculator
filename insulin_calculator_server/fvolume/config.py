231fdd8ab5fd77cae0def86fc3fb032d3a3d42e2
Frost Lee <canchen.lee@gmail.com>
2019-10-23 14:41:38 -0400

    bug fixes.

--- insulin_calculator_server/fvolume/config.py
+++ insulin_calculator_server/fvolume/config.py
@@ -10,6 +10,6 @@ CLASSIFICATION_CANDIDATES = 5
 
 RANSAC_THRESHOLD = 0.01
 
-SEG_MODEL_PATH = '/Users/Frost/Desktop/insulin_calculator/inculin_calculator_server/fvolume/model.hdf5'
+SEG_MODEL_PATH = '/Users/Frost/Desktop/insulin_calculator/insulin_calculator_server/fvolume/model.hdf5'
 
 CLASSIFIER_URL = 'https://api-2445582032290.production.gw.apicast.io/v1/foodrecognition?user_key=cf1ba06a7dcb3385b2347316408a74e8'
