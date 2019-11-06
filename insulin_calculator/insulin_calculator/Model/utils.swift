8ea0914a0b49e5984659a650e8be2274dccbfbfa
Frost Lee <canchen.lee@gmail.com>
2019-11-05 19:49:14 -0500

    bug fixes.

--- insulin_calculator/insulin_calculator/Model/utils.swift
+++ insulin_calculator/insulin_calculator/Model/utils.swift
@@ -150,26 +150,6 @@ func lensDistortionPoint(for point: CGPoint, lookupTable: Data, distortionOptica
     return CGPoint(x: opticalCenter.x + CGFloat(new_v_point_x), y: opticalCenter.y + CGFloat(new_v_point_y))
 }
 
-func buffer(from image: UIImage) -> CVPixelBuffer? {
-    let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
-    var pixelBuffer : CVPixelBuffer?
-    let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(image.size.width), Int(image.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
-    guard (status == kCVReturnSuccess) else {
-        return nil
-    }
-    CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-    let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
-    let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
-    let context = CGContext(data: pixelData, width: Int(image.size.width), height: Int(image.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
-    context?.translateBy(x: 0, y: image.size.height)
-    context?.scaleBy(x: 1.0, y: -1.0)
-    UIGraphicsPushContext(context!)
-    image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
-    UIGraphicsPopContext()
-    CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-    return pixelBuffer
-}
-
 
 /**
  Rectify the image from the lens distortion using calibration data.
@@ -202,7 +182,7 @@ func rectifyImage(
         let rectifiedRowBaseAddress = rectifiedBufferBaseAddress + row * bytesPerRow
         let rectifiedRowData = UnsafeMutableBufferPointer(start: rectifiedRowBaseAddress.assumingMemoryBound(to: UInt8.self), count: bytesPerRow)
         for col in 0 ..< width {
-            let rectifiedPoint = CGPoint(x: row, y: col)
+            let rectifiedPoint = CGPoint(x: col, y: row)
             let originalPoint = lensDistortionPoint(
                 for: rectifiedPoint,
                 lookupTable: calibration.lensDistortionLookupTable!,
