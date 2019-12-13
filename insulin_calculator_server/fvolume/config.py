c52e15be5c20b54ef8152c1aa4bed397acfdc740
Frost Lee <canchen.lee@gmail.com>
2019-12-13 17:32:27 -0500

    changed model.

--- insulin_calculator_server/fvolume/config.py
+++ insulin_calculator_server/fvolume/config.py
@@ -32,7 +32,7 @@ GRID_LEN = 2e-3
 PACKAGE_ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
 
 # The path of the food segmentation model.
-SEG_MODEL_PATH = os.path.join(PACKAGE_ROOT_PATH, *['ml_model', 'unet_samplenorm.hdf5'])
+SEG_MODEL_PATH = os.path.join(PACKAGE_ROOT_PATH, *['ml_model', 'unet.hdf5'])
 
 # The path of the shared object for image undistorting.
 UNDISTORT_DLL_PATH = os.path.join(PACKAGE_ROOT_PATH, *['c_core', 'undistort.so'])
